ab007927d98fb484e86ea3ce173d262feb1edd54
kwetso12 <kgaladikwetso@gmail.com>
2023-09-27 09:26:53 +0200

    added my mongodb code

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import 'styles/globals.css'
-import Layout from '../components/layout/layout'
+import MainNavigation from '@/components/layout/main-navigation';
+import Head from 'next/head';
+import Layout from '@/components/layout/layout';
 
-export default function App({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-   </Layout>
-}
+function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+
+export default App
